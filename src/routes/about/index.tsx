4ba03abbc44ab014f81bd33def89c72e478d82f1
deztyy <tibet2546@gmail.com>
2025-01-28 11:59:29 +0700

    update version

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,22 @@
+import { component$, useStyles$ } from "@builder.io/qwik";
+import AboutStyles from './about.css?inline'
+export default component$(() => {
+    useStyles$(AboutStyles)
+    return(
+        <article>
+            <h2>About</h2>
+            <p>
+                slkackasckcmscaskcmasc;amcasc;mca;csmca;cassalaac;av
+                sasaaacslcmac;amccmcc;lams;cacacaccsacsa
+            </p>
+            <p>
+                slkackasckcmscaskcmasc;amcasc;mca;csmca;cassalaac;av
+                sasaaacslcmac;amccmcc;lams;cacacaccsacsa
+            </p>
+            <p>
+                slkackasckcmscaskcmasc;amcasc;mca;csmca;cassalaac;av
+                sasaaacslcmac;amccmcc;lams;cacacaccsacsa
+            </p>
+        </article>
+    )
+})
---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,13 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return(
+        <article>
+            <h2>Contact</h2>
+            <p>
+                slkackasckcmscaskcmasc;amcasc;mca;csmca;cassalaac;av
+                sasaaacslcmac;amccmcc;lams;cacacaccsacsa
+            </p>
+        </article>
+    )
+})
